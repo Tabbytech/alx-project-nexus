dfbacc3e8a09e4186e232509d41b43f195d5b984
Tabbytech <tabbyccherop@gmail.com>
2025-09-28 07:59:40 +0300

    Delete alx_project_nexus directory

--- alx_project_nexus/users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import UserRegistrationView, CustomTokenObtainPairView
-from rest_framework_simplejwt.views import TokenRefreshView
-
-urlpatterns = [
-    path("register/", UserRegistrationView.as_view(), name="register"),
-    path("login/", CustomTokenObtainPairView.as_view(), name="login"),
-    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-]
